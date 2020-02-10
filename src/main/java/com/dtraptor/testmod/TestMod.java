92b2cf3c24393b1c741c711f663693eaade07114
DTRaptor <58063566+DTRaptor@users.noreply.github.com>
2020-02-10 16:12:55 +1100

    Added main mod stuff

---
+++ src/main/java/com/dtraptor/testmod/TestMod.java
@@ -0,0 +1,10 @@
+package com.dtraptor.testmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static com.dtraptor.testmod.TestMod.MODID;
+
+@Mod(MODID)
+public class TestMod {
+    public static final String MODID = "testmod";
+}
